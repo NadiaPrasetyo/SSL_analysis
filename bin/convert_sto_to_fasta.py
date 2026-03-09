7c3a8e482e05f7d9c4a893983b0a74584d824f82
NadiaPrasetyo <nadiakinsellap@gmail.com>
2026-03-09 22:18:57 +0000

    removing - and replacing headers

--- bin/convert_sto_to_fasta.py
+++ bin/convert_sto_to_fasta.py
@@ -2,13 +2,30 @@ from Bio import SeqIO
 import argparse
 
 def main (input_files):
+    fasta_files = []
 
     for files in input_files:
+        fasta_file = f"{files.replace(".sto","")}.fasta"
+        fasta_files.append(fasta_file)
             
         records = SeqIO.parse(files, "stockholm")
-        count = SeqIO.write(records, f"{files.replace(".sto","")}.fasta", "fasta")
-        print(f"Converted %i records in file {files}" % count)
+        count = SeqIO.write(records, fasta_file, "fasta")
 
+        print(f"Converted %i records in file {files}" % count)
+        
+
+    for files in fasta_files:
+        # replace the headers to include the SSL number and remove all '-' from the sequence
+        ssl_number = files.split("/")[-1].split(".")[0]
+        records = list(SeqIO.parse(files, "fasta"))
+        for record in records:
+            if not record.id.endswith(f"{ssl_number}"):
+                record.id = record.id.split(" ")[0]
+                record.id = f"{record.id}|{ssl_number}"
+            record.seq = record.seq.replace("-","")
+            record.description = ""
+        SeqIO.write(records, files, "fasta")
+          
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Convert Stockholm file into Fasta format")
