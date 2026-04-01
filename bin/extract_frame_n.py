36cb4814f9ccd9a958e29fe351d626cb41ca2f1e
NadiaPrasetyo <nadiakinsellap@gmail.com>
2026-04-01 00:26:42 +0000

    adding mmseqs fasta with selected SSLs for tcoffee alignment

--- bin/extract_frame_n.py
+++ bin/extract_frame_n.py
@@ -1,10 +1,37 @@
 from Bio import SeqIO
+from Bio.Seq import Seq
 import argparse
+import csv
+
+def tsv_file_to_fasta(tsv_file):
+    # tsv header:
+    # query	target	pident	alnlen	evalue	bits	qcov	tcov	tstart	tend	taln
+    seen_ids = set()
+    records = []
+    with open(tsv_file, "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for row in reader:
+            if row[0] == "query":
+                continue
+
+            if row[1] in seen_ids:
+                continue
+                
+            seen_ids.add(row[1])
+            record = SeqIO.SeqRecord(
+                Seq(row[10].replace("-", "")), id=row[1], description=""
+            )
+            records.append(record)
+
+    print("Number of sequences in tsv file: %i" % len(records)) # print number of records
+    return records
 
 
-def main(file, output, frame, unduplicate):
-
-    records = list(SeqIO.parse(file, "fasta"))
+def main(file, tsv_file, output, frame, unduplicate):
+    if tsv_file is not None:
+        records = tsv_file_to_fasta(tsv_file)
+    else:
+        records = list(SeqIO.parse(file, "fasta"))
     frame_records = []
 
     for record in records:
@@ -30,8 +57,14 @@ def main(file, output, frame, unduplicate):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i ", "--input", help="Input FASTA file")
-    parser.add_argument("-o", "--output", help="Output FASTA file")
+    parser.add_argument("--input-tsv", help="Input tsv file")
+    parser.add_argument("-o", "--output", default="data/extracted_frame_n.fasta",help="Output FASTA file")
     parser.add_argument("--frame", default=1, type=int, help="Frame to extract (default: 1)")
     parser.add_argument("--unduplicate", action="store_true", help="Remove duplicate sequences")
     args = parser.parse_args()
-    main(args.input, args.output, args.frame, args.unduplicate)
+
+    # check that input or input tsv has been provided
+    if args.input is None and args.input_tsv is None:
+        raise ValueError("Please provide either an input FASTA file or an input tsv file")
+
+    main(args.input, args.input_tsv, args.output, args.frame, args.unduplicate)
--- bin/nail_locus_SSLs.py
+++ bin/nail_locus_SSLs.py
@@ -4,3 +4,12 @@
 # SSL3: SAUR0421
 # SSL7: SAUR0426
 # SSL11: SAUR0420
+
+
+import subprocess
+
+subprocess.run(["nail", "search", 
+    "data/6_strains/aa/SSL_aa.fasta",
+    "data/pubMLST/pubMLST_2016-2026_summary_unique_6frame_proteins.fasta",
+    "--tbl-out", "data/pubMLST/nail_SSL_map_results.tbl",
+])
